f9a6a6884c628734b14ccca7e4bb4a26cf77f41c
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-28 22:45:24 -0800

    PEP8 fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,16 +3,18 @@
  * binary_tree_nodes -  counts the nodes with at least 1 child in a binary tree
  * @tree: a pointer to the root node of the tree to count the number of nodes
  *
- * Return: The number of nodes, 0 if its NULL
+ * Return: Number of nodes with at least one child. If tree is NULL, returns 0.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	int count;
+	int leftCount = binary_tree_nodes(tree->left);
+	int rightCount = binary_tree_nodes(tree->right);
 
 	if (tree == NULL)
 	{
-		return (0);
+		return (0); /* Base case: Empty tree */
 	}
 	count = (tree->left != NULL || tree->right != NULL) ? 1 : 0;
-	return (count + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (count + leftCount + rightCount); /* Recursive count */
 }
